8b1a3ef671ee6674fb116803f7f24907f662fbd9
Praveen Rajan <118684294+rajanpraveen@users.noreply.github.com>
2022-11-20 19:08:34 +0100

    Update streamlit_app.py
    added icons in the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard-boilded free range egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & rocket smoothie')
+streamlit.text('🐔 Hard-boilded free range egg')
+streamlit.text('🥑🍞 Avacado Toast')
